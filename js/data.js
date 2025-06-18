6435d8eaf4799498108e57401d09ff28586756a4
supreti1 <supreti1@svsu.edu>
2025-06-18 19:27:24 -0400

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,20 @@
+const products = [
+  {
+    name: "Portrait of Marten Soolmans",
+    image: "images/105070.jpg",
+    price: 75,
+    quantity: 3
+  },
+  {
+    name: "View of Houses in Delft",
+    image: "images/106060.jpg",
+    price: 125,
+    quantity: 1
+  },
+  {
+    name: "Woman Reading a Letter",
+    image: "images/106050.jpg",
+    price: 100,
+    quantity: 2
+  }
+];
